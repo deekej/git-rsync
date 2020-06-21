48f1dc2c5032ec21a56eaf45bfd9bbb9bdf5cd19
Dee'Kej <devel@deekej.io>
2020-06-21 17:05:14 +0200

    restore-files.sh script added

---
+++ .scripts/restore-files.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+
+PROG="$(basename ${0})"
+
+# -----------------------------------------------------------------------------
+
+function sysf_backup()
+{
+  if [[ ${FILES_TYPE} == system ]]; then
+    if grep -q -e '/etc/fstab' -e '/etc/default/grub' "${FILES_SYNC}"; then
+      # Make a backup of fstab & grub configs, which are often causing troubles
+      # booting up when incorrectly restored:
+      cp -a /etc/fstab        /etc/fstab.init
+      cp -a /etc/default/grub /etc/default/grub.init
+    fi
+  fi
+}
+
+function sysf_restore()
+{
+  if [[ ${FILES_TYPE} == system ]]; then
+    if [[ -e /etc/fstab.init || -e /etc/default/grub.init ]]; then
+      # Restore the initial fstab & grub config:
+      mv /etc/fstab        /etc/fstab.rpmnew
+      mv /etc/default/grub /etc/default/grub.rpmnew
+
+      mv /etc/fstab.init        /etc/fstab
+      mv /etc/default/grub.init /etc/default/grub
+
+      echo "${PROG}: INFO: Manual merging of these files is required:"
+      echo "${PROG}:        - /etc/fstab.rpmnew"
+      echo "${PROG}:        - /etc/default/grub.rpmnew"
+    fi
+
+    echo "${PROG}: INFO: Restoring SELinux context of all restored files to default"
+    echo "${PROG}:       ... This may take a while. Do NOT interrupt!"
+
+    restorecon -p -i -W -R -f "${FILES_SYNC}"
+  fi
+}
+
+# -----------------------------------------------------------------------------
+
+# Source the default/user configuration:
+source .scripts/config
+
+if [[ ${FILES_TYPE} == system ]]; then
+  # Restoring the system files requires elevated privileges:
+  if [[ ${EUID} != 0 ]]; then
+    echo "$(basename "${0}"): ERROR: elevated privileges are required"
+    exit 1
+  fi
+
+  # Some of the configuration may be hostname specific and break the system,
+  # if no hostname is set. Force the use to set the hostname:
+  if [[ "$(uname -n)" == localhost* ]]; then
+    echo "$(basename "${0}"): ERROR: hostname must be set"
+    exit 1
+  fi
+fi
+
+sysf_backup                           # Backup system files if necessary.
+
+# Restore all files metadata (except SELLinux context):
+$metadata ${META_OPTIONS} \
+          --target "${FILES_META}" \
+          --apply
+
+# Sync the backed up files into this corresponding paths:
+rsync ${RSYNC_OPTIONS} \
+      --exclude='^[[:space:]]#.*$' \
+      --files-from="${FILES_SYNC}" \
+      "${PWD}" /                      # Source : Destination
+
+sysf_restore                          # Restore system files & SELinux context.
